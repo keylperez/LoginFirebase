15085bc64071666a44639f3d824e8a886118a3c6
keylperez <perez.williamcaleb@gmail.com>
2024-11-08 09:54:28 +0800

    initial commit (submission)

---
+++ app/src/main/java/com/yuutaro/loginfirebase/LoginActivity.java
@@ -0,0 +1,85 @@
+package com.yuutaro.loginfirebase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailLogin, passLogin;
+    private Button loginBtn, registerIntentBtn;
+
+    private FirebaseAuth mAuthLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        mAuthLogin = FirebaseAuth.getInstance();
+        emailLogin = findViewById(R.id.emaillogin);
+        passLogin = findViewById(R.id.passlogin);
+        loginBtn = findViewById(R.id.loginbtn);
+        registerIntentBtn = findViewById(R.id.registerintentbtn);
+
+        registerIntentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser();
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void loginUser(){
+
+        String user, pass;
+
+        user = emailLogin.getText().toString();
+        pass = passLogin.getText().toString();
+
+        mAuthLogin
+                .signInWithEmailAndPassword(user, pass)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+    }
+}
